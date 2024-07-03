b3f0de6b52810ae1fc2cc92872f5c3ab5a54b3c2
TEMOVA <temanastasa@gmail.com>
2024-07-03 17:45:53 +0200

    error correction

--- front-end/src/CustomComponents/category.js
+++ front-end/src/CustomComponents/category.js
@@ -1,18 +1,16 @@
 import React from "react";
 
-class category extends React.Component{
-
-    render(){
-        return(
-        <div className="card" 
-             style={{margin:"10px"}}>
-            <div className="card-body">
-                <h5 className="card-title">Choose books category</h5>
-                <p className="card-text"> </p>
+class Category extends React.Component {
+    render() {
+        return (
+            <div className="card" style={{ margin: "10px" }}>
+                <div className="card-body">
+                    <h5 className="card-title">Choose books category</h5>
+                    <p className="card-text"></p>
+                </div>
             </div>
-        </div>
-        )
+        );
     }
 }
 
-export default category
+export default Category;
