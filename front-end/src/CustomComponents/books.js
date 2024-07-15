52c2e877677802feb9c6248aa17b1426fda285e8
TEMOVA <temanastasa@gmail.com>
2024-07-15 21:15:49 +0200

    Putting comment box for books

--- front-end/src/CustomComponents/books.js
+++ front-end/src/CustomComponents/books.js
@@ -3,14 +3,45 @@ import axios from "axios";
 import { API_URL } from "../Utils/Configuration";
 
 class Books extends Component {
-  //method for the file upload
-  uploadFile = (event) => {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comment: "",
+      selectedFile: null,
+      title: "" 
+    };
+  }
+
+  handleCommentChange = (event) => {
+    this.setState({ comment: event.target.value });
+  };
+
+  handleFileChange = (event) => {
+    this.setState({ selectedFile: event.target.files[0] });
+  };
+
+  handleTitleChange = (event) => {
+    this.setState({ title: event.target.value });
+  };
+
+  saveComment = () => {
+    const { comment, title } = this.state;
+    axios.post(`${API_URL}/saveComment`, { comment, title })
+      .then(res => {
+        console.log("Comment saved:", res.data);
+      })
+      .catch(err => {
+        console.error("Error saving comment:", err);
+      });
+  };
+
+  uploadFile = () => {
     const data = new FormData();
-    data.append('file', event.target.files[0]);
+    data.append('file', this.state.selectedFile);
+
     axios.post(`${API_URL}/uploadFile`, data)
       .then(res => {
-        //then for printing the response status
-        console.log(res.data);
+        console.log("File uploaded:", res.data);
       })
       .catch(err => {
         console.error("Error uploading file:", err);
@@ -20,11 +51,29 @@ class Books extends Component {
   render() {
     return (
       <div className="card" style={{ margin: "10px" }}>
-        <h3 style={{ margin: "10px" }}>File upload</h3>
+        <h3 style={{ margin: "10px" }}>Write comment for a book</h3>
         <div className="mb-3" style={{ margin: "10px" }}>
           <div className="mb-3">
-            <label htmlFor="formFile" className="form-label">Default file input example</label>
-            <input className="form-control" type="file" id="file" onChange={this.uploadFile} />
+            <label htmlFor="title" className="form-label">Name of the book</label>
+            <input
+              name="title"
+              type="text"
+              onChange={this.handleTitleChange}
+              className="form-control"
+              placeholder="Name of the book"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="comment" className="form-label">Write a comment</label>
+            <textarea
+              className="form-control"
+              id="comment"
+              rows="3"
+              value={this.state.comment}
+              onChange={this.handleCommentChange}
+              placeholder="Write a comment"
+            ></textarea>
+            <button className="btn btn-primary" onClick={this.saveComment} style={{ marginTop: "10px", backgroundColor: '#003f5c', borderColor: '#003f5c' }}>Save Comment</button>
           </div>
         </div>
       </div>
