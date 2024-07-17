a2d1f959c1c426aaf99db18e4ab9dd085afd078c
TEMOVA <temanastasa@gmail.com>
2024-07-17 17:09:59 +0200

    adding rating part

--- front-end/src/CustomComponents/books.js
+++ front-end/src/CustomComponents/books.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import StarRatings from "react-star-ratings";
 import { API_URL } from "../Utils/Configuration";
 
 class Books extends Component {
@@ -7,8 +8,9 @@ class Books extends Component {
     super(props);
     this.state = {
       comment: "",
-      selectedFile: null,
-      title: "" 
+      title: "",
+      rating: 0,
+      starColors: ["#ff4545", "#ffa534", "#ffe234", "#b7dd29", "#57e32c"]
     };
   }
 
@@ -16,17 +18,19 @@ class Books extends Component {
     this.setState({ comment: event.target.value });
   };
 
-  handleFileChange = (event) => {
-    this.setState({ selectedFile: event.target.files[0] });
-  };
-
   handleTitleChange = (event) => {
     this.setState({ title: event.target.value });
   };
 
+  changeRating = (newRating) => {
+    this.setState((prevState) => ({
+      rating: prevState.rating === newRating ? 0 : newRating
+    }));
+  };
+
   saveComment = () => {
-    const { comment, title } = this.state;
-    axios.post(`${API_URL}/saveComment`, { comment, title })
+    const { comment, title, rating } = this.state;
+    axios.post(`${API_URL}/saveComment`, { comment, title, rating })
       .then(res => {
         console.log("Comment saved:", res.data);
       })
@@ -35,20 +39,9 @@ class Books extends Component {
       });
   };
 
-  uploadFile = () => {
-    const data = new FormData();
-    data.append('file', this.state.selectedFile);
-
-    axios.post(`${API_URL}/uploadFile`, data)
-      .then(res => {
-        console.log("File uploaded:", res.data);
-      })
-      .catch(err => {
-        console.error("Error uploading file:", err);
-      });
-  };
-
   render() {
+    const { rating, starColors } = this.state;
+
     return (
       <div className="card" style={{ margin: "10px" }}>
         <h3 style={{ margin: "10px" }}>Write comment for a book</h3>
@@ -73,8 +66,29 @@ class Books extends Component {
               onChange={this.handleCommentChange}
               placeholder="Write a comment"
             ></textarea>
-            <button className="btn btn-primary" onClick={this.saveComment} style={{ marginTop: "10px", backgroundColor: '#003f5c', borderColor: '#003f5c' }}>Save Comment</button>
           </div>
+          <div className="mb-3">
+            <label htmlFor="rating" className="form-label">Rate the book</label>
+            <div style={{ fontSize: 24 }}>
+              <StarRatings
+                rating={rating}
+                starRatedColor={rating ? starColors[rating - 1] : starColors[0]}
+                changeRating={this.changeRating}
+                numberOfStars={5}
+                name='rating'
+                starDimension="40px"
+                starSpacing="15px"
+                starHoverColor={starColors[0]}
+              />
+            </div>
+          </div>
+          <button
+            className="btn btn-primary"
+            onClick={this.saveComment}
+            style={{ marginTop: "10px", backgroundColor: '#003f5c', borderColor: '#003f5c' }}
+          >
+            Save comment and rating
+          </button>
         </div>
       </div>
     );
