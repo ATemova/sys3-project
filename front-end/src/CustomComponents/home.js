130d37dbf6477677c6d46a37affb1696366ace17
TEMOVA <temanastasa@gmail.com>
2024-08-07 13:41:03 +0200

    Adding comments

--- front-end/src/CustomComponents/home.js
+++ front-end/src/CustomComponents/home.js
@@ -4,8 +4,9 @@ import { API_URL } from "../Utils/Configuration";
 class Home extends React.Component {
   constructor(props) {
     super(props);
+    // Initialize component state
     this.state = {
-      userName: "",
+      userName: "", // Stores the user's name fetched from the API
     };
   }
 
@@ -13,19 +14,21 @@ class Home extends React.Component {
   componentDidMount() {
     // Fetch user data from the API
     fetch(`${API_URL}/user`)
-      .then((response) => response.json())
+      .then((response) => response.json()) // Parse the response as JSON
       .then((data) => {
         if (data && data.name) {
+          // Update the state with the user's name
           this.setState({ userName: data.name });
         }
       })
-      .catch((error) => console.error("Error fetching user data:", error));
+      .catch((error) => console.error("Error fetching user data:", error)); // Handle any errors
   }
 
   render() {
     return (
       <div className="card" style={{ margin: "10px" }}>
         <div className="card-body">
+          {/* Display a welcome message with the user's name */}
           <h5 className="card-title">Welcome, {this.state.userName}!</h5>
           <p className="card-text">You are in the online University library</p>
         </div>
